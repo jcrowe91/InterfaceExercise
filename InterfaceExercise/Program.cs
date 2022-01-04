31fd0f325fab0f021a9e1660c5cdd09cc7fc975f
jcrowe91 <jcrowe9119@gmail.com>
2022-01-03 19:54:50 -0600

    Classes Created

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -9,9 +9,9 @@ namespace InterfaceExercise
 
             //TODO Be sure to follow BEST PRACTICES when creating classes and interfaces
 
-            //Create 2 Interfaces called IVehicle & ICompany
+            //DONECreate 2 Interfaces called IVehicle & ICompany
 
-            //Create 3 classes called Car , Truck , & SUV
+            //DONECreate 3 classes called Car , Truck , & SUV
 
             //In your IVehicle
             
